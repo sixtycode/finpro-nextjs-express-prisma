4619efffe8ce9c3d79269a3753de34fc3ca38017
sixtycode <sagaraperkasa.work@gmail.com>
2024-11-30 01:50:37 +0700

    refactor: landing page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,27 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Landing Page</div>;
+  return (
+    <>
+      <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold">Hello Fellas!</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+            Go Discover Event
+          </h1>
+          <p className="mt-6 text-base leading-7">
+            Refresh mind from terrific routines.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid bg-black px-4 text-sm text-white transition-colors hover:bg-[#383838] hover:text-white sm:h-12 sm:px-5 sm:text-base dark:bg-transparent dark:text-white dark:hover:bg-[#ccc] dark:hover:text-black"
+              href="/event"
+            >
+              READY!
+            </Link>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }

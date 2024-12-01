c396f270436662f8a7a1bf9ff520e811e3306c29
Vio Sagara Perkasa <96647754+sixtycode@users.noreply.github.com>
2024-12-01 21:19:21 +0700

    feat: 2.1 checkpoint three (#60)
    * feat: 2.1 Account Work In Progress
    
    * feat: 2.2 Referral System Work In Progress (#57)
    
    * feat: 2.3 Organizer Dashboard Work In Progress (#58)
    
    * feat: user admin Work In Progress
    
    * feat: admin login page
    
    * feat: 2.1 Login and Recovery Page WIP
    
    * style: footer visited color removal

--- apps/web/src/app/admin/sign/page.tsx
+++ apps/web/src/app/admin/sign/page.tsx
@@ -2,24 +2,18 @@ import Link from 'next/link';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Sign In',
-  description: 'Enjoy the Show',
+  title: 'Administrator Sign',
+  description: 'Payment Verification',
 };
 
-export default function Login() {
+export default function Sign() {
   return (
     <>
       <div className="mx-auto flex flex-col items-center justify-center px-6 py-8">
         <div className="w-full sm:max-w-md md:mt-0 xl:p-0">
           <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
             <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl">
-              one step closer,{' '}
-              <Link
-                href="/register"
-                className="text-blue-600 underline underline-offset-4 hover:text-blue-800 dark:text-blue-100 dark:hover:text-blue-200"
-              >
-                does it?
-              </Link>
+              Admin SSO
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
@@ -33,15 +27,7 @@ export default function Login() {
                 ></input>
               </div>
               <div>
-                <div className="mb-2 flow-root text-sm font-medium">
-                  <div className="float-left">Password</div>
-                  <Link
-                    href="/recovery"
-                    className="float-right font-mono font-bold text-blue-600 underline underline-offset-4 hover:text-blue-800 dark:text-blue-100 dark:hover:text-blue-200"
-                  >
-                    i forgor
-                  </Link>
-                </div>
+                <div className="mb-2 block text-sm font-medium">Password</div>
                 <input
                   type="password"
                   name="password"

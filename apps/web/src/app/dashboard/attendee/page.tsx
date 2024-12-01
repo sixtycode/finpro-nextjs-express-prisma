884e1812c852df31b8a0a85a4975e6b644f59a93
Vio Sagara Perkasa <96647754+sixtycode@users.noreply.github.com>
2024-12-02 00:33:12 +0700

    feat: 2.3 Event Management Dashboard WIP (#61)
    * feat: 2.3 Organizer Dashboard Work In Progress
    
    * feat: 2.1 user admin Work In Progress (#59)
    
    * feat: 2.1 Account Work In Progress
    
    * feat: 2.2 Referral System Work In Progress (#57)
    
    * feat: 2.3 Organizer Dashboard Work In Progress (#58)
    
    * feat: user admin Work In Progress
    
    * feat: 2.1 checkpoint three (#60)
    
    * feat: 2.1 Account Work In Progress
    
    * feat: 2.2 Referral System Work In Progress (#57)
    
    * feat: 2.3 Organizer Dashboard Work In Progress (#58)
    
    * feat: user admin Work In Progress
    
    * feat: admin login page
    
    * feat: 2.1 Login and Recovery Page WIP
    
    * style: footer visited color removal
    
    * feat: 2.3 Event Management Dashboard WIP

---
+++ apps/web/src/app/dashboard/attendee/page.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Organizer Attendee',
+  description: 'Data Insight',
+};
+
+export default function Attendee() {
+  return (
+    <>
+      <div className="grid min-h-full place-items-center px-6 2xl:px-80">
+        <Link
+          href="/dashboard"
+          className="mb-4 w-full rounded-lg border-2 border-black py-3 text-center dark:border-white"
+        >
+          Dashboard
+        </Link>
+      </div>
+      <div className="grid min-h-full px-6 text-xl font-bold 2xl:px-80">
+        Attendee Registrations
+      </div>
+    </>
+  );
+}
--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { Metadata } from 'next';
+import { itemsDash } from './dashItem';
 
 export const metadata: Metadata = {
   title: 'Organizer Dashboard',
@@ -9,25 +10,19 @@ export const metadata: Metadata = {
 export default function Dashboard() {
   return (
     <>
-      <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center">
-          <p className="text-base font-semibold">Hello Fellas!</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
-            Go Discover Event
-          </h1>
-          <p className="mt-6 text-base leading-7">
-            Refresh mind from terrific routines.
-          </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
+      <div className="grid min-h-full place-items-center px-6 py-24 sm:py-32 2xl:px-80">
+        {itemsDash.map((item) => {
+          return (
             <Link
-              className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid bg-black px-4 text-sm text-white transition-colors hover:bg-[#383838] hover:text-white sm:h-12 sm:px-5 sm:text-base dark:bg-transparent dark:text-white dark:hover:bg-[#ccc] dark:hover:text-black"
-              href="/event"
+              href={item.link}
+              key={item.link}
+              className="mb-4 w-full rounded-lg border-2 border-black py-3 text-center dark:border-white"
             >
-              READY!
+              {item.name}
             </Link>
-          </div>
-        </div>
-      </main>
+          );
+        })}
+      </div>
     </>
   );
 }

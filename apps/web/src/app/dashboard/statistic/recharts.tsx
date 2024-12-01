5561c4c7a0e5b44dfb0eaaf9fc1642b4588c7e29
Vio Sagara Perkasa <96647754+sixtycode@users.noreply.github.com>
2024-12-02 00:33:12 +0700

    feat: 2.3 Event Management Dashboard WIP (#61)
    * feat: 2.3 Organizer Dashboard Work In Progress
    
    * feat: 2.1 user admin Work In Progress (#59)
    
    * feat: 2.1 Account Work In Progress
    
    * feat: 2.2 Referral System Work In Progress (#57)
    
    * feat: 2.3 Organizer Dashboard Work In Progress (#58)
    
    * feat: user admin Work In Progress
    
    * feat: 2.1 checkpoint three (#60)
    
    * feat: 2.1 Account Work In Progress
    
    * feat: 2.2 Referral System Work In Progress (#57)
    
    * feat: 2.3 Organizer Dashboard Work In Progress (#58)
    
    * feat: user admin Work In Progress
    
    * feat: admin login page
    
    * feat: 2.1 Login and Recovery Page WIP
    
    * style: footer visited color removal
    
    * feat: 2.3 Event Management Dashboard WIP

---
+++ apps/web/src/app/dashboard/statistic/recharts.tsx
@@ -0,0 +1,143 @@
+'use client';
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+
+export default function Recharts() {
+  const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+  return (
+    <>
+      <div className="py-4 text-black" style={{ width: '100%' }}>
+        <h4 className="dark:text-white">Yearly Range</h4>
+        <ResponsiveContainer width="100%" height={200} className="bg-white">
+          <AreaChart
+            width={500}
+            height={200}
+            data={data}
+            syncId="anyId"
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="uv"
+              stroke="#8884d8"
+              fill="#8884d8"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+        <p className="mt-8 dark:text-white">Monthly Range</p>
+
+        <ResponsiveContainer width="100%" height={200} className="bg-white">
+          <AreaChart
+            width={500}
+            height={200}
+            data={data}
+            syncId="anyId"
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="pv"
+              stroke="#82ca9d"
+              fill="#82ca9d"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+        <p className="mt-8 dark:text-white">Daily Range</p>
+
+        <ResponsiveContainer width="100%" height={200} className="bg-white">
+          <AreaChart
+            width={500}
+            height={200}
+            data={data}
+            syncId="anyId"
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="amt"
+              stroke="#ffc658"
+              fill="#ffc658"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+    </>
+  );
+}

a42693ca30967c90bd466a796c0c763ef3a4b0f4
Vio Sagara Perkasa <96647754+sixtycode@users.noreply.github.com>
2024-12-02 17:54:56 +0700

    refactor: 2.1 checkpoint (#69)
    * feat: 2.1 Account Work In Progress
    
    * feat: 2.2 Referral System Work In Progress (#57)
    
    * feat: 2.3 Organizer Dashboard Work In Progress (#58)
    
    * feat: user admin Work In Progress
    
    * feat: admin login page
    
    * feat: 2.1 Login and Recovery Page WIP
    
    * style: footer visited color removal
    
    * feat: 2.3 Event Management Dashboard WIP (#61)
    
    * feat: 2.3 Organizer Dashboard Work In Progress
    
    * feat: 2.1 user admin Work In Progress (#59)
    
    * feat: 2.1 Account Work In Progress
    
    * feat: 2.2 Referral System Work In Progress (#57)
    
    * feat: 2.3 Organizer Dashboard Work In Progress (#58)
    
    * feat: user admin Work In Progress
    
    * feat: 2.1 checkpoint three (#60)
    
    * feat: 2.1 Account Work In Progress
    
    * feat: 2.2 Referral System Work In Progress (#57)
    
    * feat: 2.3 Organizer Dashboard Work In Progress (#58)
    
    * feat: user admin Work In Progress
    
    * feat: admin login page
    
    * feat: 2.1 Login and Recovery Page WIP
    
    * style: footer visited color removal
    
    * feat: 2.3 Event Management Dashboard WIP
    
    * refactor: 2.1 profile picture placeholder
    
    * refactor: 2.1 Register and Recovery Page
    
    * style: 2.1 readability accessibility
    
    * refactor: 2.1 checkpoint

--- apps/web/src/app/profile/page.tsx
+++ apps/web/src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { Metadata } from 'next';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: 'Profile',
@@ -9,24 +10,39 @@ export const metadata: Metadata = {
 export default function Profile() {
   return (
     <>
+      <div className="fixed bottom-2">
+        <Link
+          href="/dashboard"
+          className="relative ml-3 flex size-8 rounded-full border-2 bg-transparent text-sm"
+        ></Link>
+      </div>
+      <div className="fixed bottom-2 right-2">
+        <Link
+          href="/admin/sign/"
+          className="relative ml-3 flex size-8 rounded-full border-2 bg-transparent text-sm"
+        ></Link>
+      </div>
       <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center">
-          <p className="text-base font-semibold">Hello Fellas!</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
-            Go Discover Event
-          </h1>
-          <p className="mt-6 text-base leading-7">
-            Refresh mind from terrific routines.
-          </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
+        <div className="">
+          <div>
+            <Image
+              src="/Profile_avatar_placeholder_large.png"
+              width={256}
+              height={256}
+              alt="Profile Picture"
+              className="size-36 rounded-full bg-red-400 text-sm"
+            ></Image>
+            <div className="mt-4">John Doe</div>
+            <div className="mb-4">johndoe@outlook.com</div>
+            <div className="mt-4">Points: 30000 Expired 01 January 2025</div>
+            <div className="mb-4">Referral Codes: JHND3479</div>
             <Link
               className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid bg-black px-4 text-sm text-white transition-colors hover:bg-[#383838] hover:text-white sm:h-12 sm:px-5 sm:text-base dark:bg-transparent dark:text-white dark:hover:bg-[#ccc] dark:hover:text-black"
-              href="/event"
+              href="/login"
             >
-              READY!
+              Sign Out
             </Link>
           </div>
-          <h1>Referral Points</h1>
         </div>
       </main>
     </>

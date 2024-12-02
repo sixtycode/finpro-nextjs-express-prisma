d2840f22a527c29d52854d6ae8f28d678ae1cf60
sixtycode <sagaraperkasa.work@gmail.com>
2024-12-02 15:44:13 +0700

    refactor: 2.1 Register and Recovery Page

--- apps/web/src/app/register/page.tsx
+++ apps/web/src/app/register/page.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import { Metadata } from 'next';
+import FormRegister from './formRegister';
 
 export const metadata: Metadata = {
   title: 'Sign Up',
@@ -9,25 +9,13 @@ export const metadata: Metadata = {
 export default function Register() {
   return (
     <>
-      <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center">
-          <p className="text-base font-semibold">Hello Fellas!</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
-            Go Discover Event
-          </h1>
-          <p className="mt-6 text-base leading-7">
-            Refresh mind from terrific routines.
-          </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Link
-              className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid bg-black px-4 text-sm text-white transition-colors hover:bg-[#383838] hover:text-white sm:h-12 sm:px-5 sm:text-base dark:bg-transparent dark:text-white dark:hover:bg-[#ccc] dark:hover:text-black"
-              href="/event"
-            >
-              READY!
-            </Link>
+      <div className="mx-auto flex flex-col items-center justify-center px-6 py-8">
+        <div className="w-full sm:max-w-md md:mt-0 xl:p-0">
+          <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
+            <FormRegister></FormRegister>
           </div>
         </div>
-      </main>
+      </div>
     </>
   );
 }
